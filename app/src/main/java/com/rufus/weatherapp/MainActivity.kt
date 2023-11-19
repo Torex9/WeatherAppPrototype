a7dedf75f53bcacad791a45f528e1c22783ba531
Torex9 <92604396+Torex9@users.noreply.github.com>
2023-11-19 23:10:59 +0000

    Layout of app

--- app/src/main/java/com/rufus/weatherapp/MainActivity.kt
+++ app/src/main/java/com/rufus/weatherapp/MainActivity.kt
@@ -10,7 +10,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,8 +27,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -31,6 +46,8 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.rufus.weatherapp.constant.Const.Companion.colorBg1
+import com.rufus.weatherapp.constant.Const.Companion.colorBg2
 import com.rufus.weatherapp.constant.Const.Companion.permissions
 import com.rufus.weatherapp.model.MyLatLng
 import com.rufus.weatherapp.ui.theme.WeatherAppTheme
@@ -154,7 +171,42 @@ class MainActivity : ComponentActivity() {
             }
         })
 
-        Text(text = "${currentLocation.lat}/${currentLocation.lng}")
+        val gradient = Brush.linearGradient(
+            colors = listOf(Color(colorBg1) , Color(colorBg2)),
+            start = Offset(1000f, -1000f),
+            end = Offset(1000f, 1000f)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradient)
+        ) {
+            val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+            val marginTop = screenHeight * 0.2f // I want the margin top at 20% height
+            val marginTopPx = with(LocalDensity.current) {marginTop.toPx()}
+
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+
+                        //Define the layout for the child
+                        layout(
+                            placeable.width,
+                            placeable.height + marginTopPx.toInt()
+                        ) {
+                            placeable.placeRelative(0, marginTopPx.toInt())
+                        }
+                    },
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hello")
+            }
+
+        }
     }
 
     private fun initlocationClient() {
--- app/src/main/java/com/rufus/weatherapp/constant/Const.kt
+++ app/src/main/java/com/rufus/weatherapp/constant/Const.kt
@@ -6,5 +6,13 @@ class Const {
             android.Manifest.permission.ACCESS_COARSE_LOCATION,
             android.Manifest.permission.ACCESS_FINE_LOCATION
         )
+
+        const val  openWeatherMapApiKey = "34a2af3c090ed1efc0bb8ee92cfac078";
+        //const val  openWeatherMapApiKey = "ae6170a64d465935e51cee2d1d45fc8d";
+        const val  colorBg1 = 0xff08203e;
+        const val  colorBg2 = 0xff557c93;
+
+        const val  LOADING = "Loading..."
+        const val  NA= "N/A"
     }
 }
