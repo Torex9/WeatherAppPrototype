bd6667128bcbd469030fa75ce4bedf096f3806ab
Torex9 <92604396+Torex9@users.noreply.github.com>
2023-12-02 05:10:48 +0000

    Splash Screen Updated

--- app/src/main/java/com/rufus/weatherapp/MainActivity.kt
+++ app/src/main/java/com/rufus/weatherapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
+import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import android.window.SplashScreen
 import androidx.activity.ComponentActivity
@@ -12,8 +13,10 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,6 +25,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -41,6 +45,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -48,6 +54,7 @@ import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -70,7 +77,9 @@ import com.rufus.weatherapp.constant.Const.Companion.permissions
 import com.rufus.weatherapp.model.MyLatLng
 import com.rufus.weatherapp.model.forecast.ForecastResult
 import com.rufus.weatherapp.model.weather.WeatherResult
+import com.rufus.weatherapp.ui.theme.Purple80
 import com.rufus.weatherapp.ui.theme.WeatherAppTheme
+import com.rufus.weatherapp.ui.theme.purple_700
 import com.rufus.weatherapp.view.ForecastSection
 import com.rufus.weatherapp.view.WeatherSection
 import com.rufus.weatherapp.viewmodel.MainViewModel
@@ -338,23 +347,57 @@ class MainActivity : ComponentActivity() {
     //splash screen
     @Composable
     fun SplashScreen(navController: NavController) {
+        val gradient = Brush.linearGradient(
+            colors = listOf(Color(colorBg1) , Color(colorBg2)),
+            start = Offset(1000f, -1000f),
+            end = Offset(1000f, 1000f)
+        )
         val scale = remember{ androidx.compose.animation.core.Animatable(0f) }
         LaunchedEffect(key1 = true) {
+            scale.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 800,
+                    easing = {
+                        OvershootInterpolator(4f).getInterpolation(it)
+                    }
+                )
+            )
             delay(3000L)
-            navController.navigate("main_screen")
+            navController.navigate("main_screen") {
+                popUpTo("splash_screen") {
+                    inclusive = true
+                }
+            }
         }
+
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradient),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
-            Text(text = "RUFUS WEATHER APP")
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                "",
+
+
+            Text(
+                text = "WEATHER APP",
                 modifier = Modifier
-                    .width(400.dp)
-                    .height(900.dp)
+                    .scale(scale.value),
+                color = Color.White,
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace
+            )
+            Text(
+                text = "By Rufus C2676906",
+                modifier = Modifier
+                    .scale(scale.value),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace
             )
         }
     }
